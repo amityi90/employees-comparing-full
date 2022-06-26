97a669038839ccf005a0138e4397eba0a0ecf1b7
Amit Mottes <amityi90@gmail.com>
2022-06-26 11:45:01 +0300

    feat: the full project

---
+++ src/components/EmployeesList/EmployeesList.js
@@ -0,0 +1,43 @@
+import EmployeeCheckbox from '../EmployeeCheckbox/EmployeeCheckbox';
+import Preloader from '../Preloader/Preloader';
+
+function EmployeesList(props) {
+
+    // manage the employees list and who need to be compared
+    function toggleEmployee(evt) {
+        const employees = document.querySelectorAll('.employees-checkbox__checkbox');
+        const checkedEmployees = Array.from(employees).filter(employee => { return employee.checked; });
+        if (checkedEmployees.length >= 2) {
+            employees.forEach(employee => {
+                if (!employee.checked) {
+                    employee.disabled = true;
+                }
+            })
+        } else {
+            employees.forEach(employee => { employee.disabled = false; })
+        }
+        props.assignEmployees(props.list[checkedEmployees[0]?.id] || null, props.list[checkedEmployees[1]?.id] || null);
+    }
+
+    return (
+        <section className="employees-list">
+            {props.list.length === 0 ?
+                <Preloader modifier='preloader_big' /> :
+                <ul className='employees-list__list'>
+                    {props.list.map((employee, i) => {
+                        return (
+                            <EmployeeCheckbox
+                                key={i}
+                                employee={employee}
+                                toggleEmployee={toggleEmployee}
+                                index={i}
+                                assignEmployeeToUpdate={props.assignEmployeeToUpdate}
+                            />
+                        );
+                    })}
+                </ul>}
+        </section>
+    );
+}
+
+export default EmployeesList;
---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,7 @@
+function Preloader(props) {
+    return (
+            <i className={`preloader ${props.modifier}`} />
+    );
+}
+
+export default Preloader;
